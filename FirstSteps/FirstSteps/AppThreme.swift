c4faacbea07b934fd8a6ba1d208970c2903b412a
Vladislav Zyuzko <zyuzko142@gmail.com>
2023-11-13 23:22:02 -0800

    Добавил локализацию строк

--- FirstSteps/FirstSteps/AppThreme.swift
+++ FirstSteps/FirstSteps/AppThreme.swift
@@ -21,16 +21,25 @@ struct AppTheme {
     }
     
     struct Fonts {
-        static let poppins_bold_16 = Font.custom("Poppins-Bold", size: 16)
-        static let poppins_bold_20 = Font.custom("Poppins-Bold", size: 20)
         
-        static let poppins_semibold_16 = Font.custom("Poppins-SemiBold", size: 16)
+        static let poppins_bold_16 = Font.custom(
+            "Poppins-Bold_Font".localized, size: 16)
+        static let poppins_bold_20 = Font.custom(
+            "Poppins-Bold_Font".localized, size: 20)
         
-        static let poppins_regular_12 = Font.custom("Poppins-Regular", size: 12)
-        static let poppins_regular_14 = Font.custom("Poppins-Regular", size: 14)
-        static let poppins_regular_15 = Font.custom("Poppins-Regular", size: 15)
-        static let poppins_regular_16 = Font.custom("Poppins-Regular", size: 16)
+        static let poppins_semibold_16 = Font.custom(
+            "Poppins-SemiBold_Font".localized, size: 16)
         
-        static let nunito_bold_14 = Font.custom("Nunito-Bold", size: 14)
+        static let poppins_regular_12 = Font.custom(
+            "Poppins-Regular_Font".localized, size: 12)
+        static let poppins_regular_14 = Font.custom(
+            "Poppins-Regular_Font".localized, size: 14)
+        static let poppins_regular_15 = Font.custom(
+            "Poppins-Regular_Font".localized, size: 15)
+        static let poppins_regular_16 = Font.custom(
+            "Poppins-Regular_Font".localized, size: 16)
+        
+        static let nunito_bold_14 = Font.custom(
+            "Nunito-Bold_Font".localized, size: 14)
     }
 }
